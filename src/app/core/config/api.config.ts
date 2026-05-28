bb04149a5c3870da08f717a38e2708382fd2ed5d
Yudi Cruz <karinaguayara592@gmail.com>
2026-05-27 23:12:28 -0500

    Fix API URL to production

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  BASE_URL: 'http://localhost:8084',
+  BASE_URL: 'https://aquabalance-backend-btvr.onrender.com',
   clima: 'https://api.open-meteo.com/v1/forecast'
 };
